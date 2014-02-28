9d52aeb028849d424b65ec6122983de4f4d43666
Didi Hoffmann <didi@rebelproject.org>
2014-02-28 14:59:35 +0100

    Added help note on how to install

--- README.txt
+++ README.txt
@@ -2,3 +2,7 @@ markdown.highlight
 ==================
 
 Adds the possibility to use "??? something ???" to create a span that looks like <mark>something</mark>
+
+Install through pip:
+
+$ pip install MarkdownHighlight
--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 
 setup(
     name='MarkdownHighlight',
-    version='0.1',
+    version='0.1.1',
     packages=['MarkdownHighlight',],
     license='Apache License 2.0',
     long_description=open('README.txt').read(),
     url='https://github.com/ribalba/markdown.highlight',
-    description='A markdown extension that enables you to create <mark> text by using ??? text ???',
+    description='A markdown extension that enables you to create <mark> tag by using ???some text???',
     author='Didi Hoffmann',
     author_email='didi@ribalba.de',
     install_requires=[
