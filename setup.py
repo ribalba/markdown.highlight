fdabce0ec5a3ddc8147a55e313c46c5769933627
Bruce <33303709+bduplanty@users.noreply.github.com>
2021-09-01 20:27:18 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version='0.1.3',
     packages=['MarkdownHighlight',],
     license='Apache License 2.0',
-    long_description=open('README.txt').read(),
+    long_description=open('README.md').read(),
     url='https://github.com/ribalba/markdown.highlight',
     description='A markdown extension that enables you to create <mark> tag by using ???some text???',
     author='Didi Hoffmann',
